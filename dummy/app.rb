cc7627f5bd556363eba27e2919ab092e9ca3480b
Brad Chase <brad@webappfromscratch.com>
2014-05-05 21:34:44 -0400

    dummy app for route params

--- dummy/app.rb
+++ dummy/app.rb
@@ -1,11 +1,67 @@
 require 'podunk'
 
 class Application < Podunk::App
+  class Joke
+    @@jokes = []
+
+    attr_accessor :text
+    def initialize(text)
+      @text = text
+
+      @jokes << self
+    end
+
+    def self.all
+      @@jokes
+    end
+  end
+
   route do
-    get '/' => 'home'
+    get '/'          => 'home'
+    get '/jokes'     => 'all_jokes'
+    get '/jokes/:id' => 'joke'
   end
 
   def home
     "Welcome home, #{params['name']}."
+  end
+
+  def all_jokes
+    @jokes = Joke.all
+    jokes_ul
+  end
+  
+  def joke
+    @joke = Joke.find params[:id]
+    joke_div
+  end
+
+private
+  def jokes_ul
+    ul  = "<ul>"
+
+    @jokes.each do |joke|
+      ul += joke_li(joke)
+    end
+
+    ul += "</ul>" 
+  end
+
+  def joke_li(joke)
+    "<li>#{joke.text}</li>"
+  end
+
+  def joke_div
+    "<div>"  +
+      dl('text', @joke.text) +
+      dl('source', @joke.source) +
+    "</div>"
+  end
+
+  def dl(term, description)
+    "<dl>" +
+    "  <dt>#{term}</dt>" +
+    "  <dd>#{description}</dd>" +
+    "</dl>"
   end
 end
